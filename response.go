cdc37ade1e67ae5467dd37463c8ecb05faad377d
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2022-09-29 12:47:10 -0400

    parse response

--- response.go
+++ response.go
@@ -1,6 +1,12 @@
 package vault
 
-import "github.com/hashicorp/vault/sdk/logical"
+import (
+	"bytes"
+	"io"
+	"time"
+
+	"github.com/hashicorp/vault/sdk/helper/jsonutil"
+)
 
 // Secret is the structure returned for every secret within Vault.
 type Response[T any] struct {
@@ -16,20 +22,21 @@ type Response[T any] struct {
 	Data T `json:"data"`
 
 	// Warnings contains any warnings related to the operation. These
-	// are not issues that caused the command to fail, but that the
+	// are not issues that caused the command to fail, but things that the
 	// client should be aware of.
 	Warnings []string `json:"warnings"`
 
 	// Auth, if non-nil, means that there was authentication information
 	// attached to this response.
-	Auth *SecretAuth `json:"auth,omitempty"`
+	Auth *ResponseAuth `json:"auth,omitempty"`
 
 	// WrapInfo, if non-nil, means that the initial response was wrapped in the
 	// cubbyhole of the given token (which has a TTL of the given number of
 	// seconds)
-	WrapInfo *SecretWrapInfo `json:"wrap_info,omitempty"`
+	WrapInfo *ResponseWrapInfo `json:"wrap_info,omitempty"`
 }
 
+// SecretAuth is the structure containing auth information if we have it.
 type ResponseAuth struct {
 	ClientToken      string            `json:"client_token"`
 	Accessor         string            `json:"accessor"`
@@ -43,5 +50,53 @@ type ResponseAuth struct {
 	LeaseDuration int  `json:"lease_duration"`
 	Renewable     bool `json:"renewable"`
 
-	MFARequirement *logical.MFARequirement `json:"mfa_requirement"`
+	MFARequirement *MFARequirement `json:"mfa_requirement,omitempty"`
+}
+
+// ResponseWrapInfo contains wrapping information if we have it. If what is
+// contained is an authentication token, the accessor for the token will be
+// available in WrappedAccessor.
+type ResponseWrapInfo struct {
+	Token           string    `json:"token"`
+	Accessor        string    `json:"accessor"`
+	TTL             int       `json:"ttl"`
+	CreationTime    time.Time `json:"creation_time"`
+	CreationPath    string    `json:"creation_path"`
+	WrappedAccessor string    `json:"wrapped_accessor"`
+}
+
+type MFARequirement struct {
+	MFARequestID   string                      `json:"mfa_request_id,omitempty"`
+	MFAConstraints map[string]MFAConstraintAny `json:"mfa_constraints,omitempty"`
+}
+
+type MFAConstraintAny struct {
+	Any []MFAMethodID `json:"any,omitempty"`
+}
+
+type MFAMethodID struct {
+	Type         string `json:"type,omitempty"`
+	ID           string `json:"id,omitempty"`
+	UsesPasscode bool   `json:"uses_passcode,omitempty"`
+}
+
+func ParseResponse[T any](r io.Reader) (*Response[T], error) {
+	// First read the data into a buffer. Not super efficient but we want to
+	// know if we actually have a body or not.
+	var buf bytes.Buffer
+	_, err := buf.ReadFrom(r)
+	if err != nil {
+		return nil, err
+	}
+	if buf.Len() == 0 {
+		return nil, nil
+	}
+
+	// First decode the JSON into a map[string]interface{}
+	var response Response[T]
+	if err := jsonutil.DecodeJSONFromReader(&buf, &secret); err != nil {
+		return nil, err
+	}
+
+	return &response, nil
 }

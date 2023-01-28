43d069980d2755f48c8aee4df6fede05172fc160
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-01-27 20:51:55 -0500

    split into client_option_test.go

--- client_test.go
+++ client_test.go
@@ -1,68 +1,12 @@
 package vault
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-func Test_Client_New(t *testing.T) {
-	cases := map[string]struct {
-		options []ClientOption
-	}{
-		"empty": {
-			options: nil,
-		},
-		"with-base-address": {
-			options: []ClientOption{WithBaseAddress("http://test")},
-		},
-		"with-base-address-with-request-timeout": {
-			options: []ClientOption{WithBaseAddress("http://test"), WithRequestTimeout(30 * time.Second)},
-		},
-		"with-enforce-read-your-writes-consistency": {
-			options: []ClientOption{WithEnforceReadYourWritesConsistency()},
-		},
-		"with-retry-configuration": {
-			options: []ClientOption{WithRetryConfiguration(RetryConfiguration{
-				RetryWaitMin: 200 * time.Millisecond,
-				RetryWaitMax: 900 * time.Millisecond,
-			})},
-		},
-	}
-
-	for name, tc := range cases {
-		t.Run(name, func(t *testing.T) {
-			client, err := New(tc.options...)
-
-			require.NoError(t, err)
-
-			// unset the base client pointers since they are always different
-			configurationDefault := DefaultConfiguration()
-			configurationDefault.BaseClient = nil
-
-			configurationClient := client.Configuration()
-			configurationClient.BaseClient = nil
-
-			// check if the configurations were modified
-			if len(tc.options) == 0 {
-				require.Equal(
-					t,
-					fmt.Sprintf("%v", configurationDefault),
-					fmt.Sprintf("%v", configurationClient),
-				)
-			} else {
-				require.NotEqual(
-					t,
-					fmt.Sprintf("%v", configurationDefault),
-					fmt.Sprintf("%v", configurationClient),
-				)
-			}
-		})
-	}
-}
 
 func Test_Client_Clone(t *testing.T) {
 	client, err := New(

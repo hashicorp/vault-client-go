4949f2d33f1924400ae622b7f95bfa1d4dacd984
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-04-21 17:28:17 -0400

    typo

--- api_helpers.go
+++ api_helpers.go
@@ -2,7 +2,7 @@ package vault
 
 import "fmt"
 
-// parameterToString is a very simple function that returns a string
+// parameterToString is a very simple function that returns the string
 // representation of the given parameter value. We may need to expand this
 // later to handle time.Time values, slices, etc.
 func parameterToString(value interface{}) string {

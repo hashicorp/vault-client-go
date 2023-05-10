a226df7d966c2aa7a82ad71a00bd068a3a15916e
Anton Averchenkov <84287187+averche@users.noreply.github.com>
2023-05-10 14:42:48 -0400

    Update client_requests.go
    Co-authored-by: Daniel Huckins <dhuckins@users.noreply.github.com>

--- client_requests.go
+++ client_requests.go
@@ -157,7 +157,7 @@ func sendStructuredRequestParseResponse[ResponseT any](
 	client *Client,
 	method string,
 	path string,
-	body interface{},
+	body any,
 	parameters url.Values,
 	requestModifiersPerRequest requestModifiers,
 ) (*Response[ResponseT], error) {

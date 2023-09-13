8fe17e2c5ced0ce477283464a1633511d60b5d7e
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-09-13 12:54:36 -0400

    Populate default retry configuration options

--- client_test.go
+++ client_test.go
@@ -4,6 +4,7 @@
 package vault
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -29,7 +30,7 @@ func Test_Client_Clone(t *testing.T) {
 
 	clone := client.Clone()
 
-	assert.Equal(t, client, clone)
+	testEqual(t, client, clone)
 
 	assert.Equal(t, "http://test", clone.Configuration().Address)
 	assert.Equal(t, 30*time.Second, clone.Configuration().RequestTimeout)
@@ -37,4 +38,14 @@ func Test_Client_Clone(t *testing.T) {
 	assert.Equal(t, 900*time.Millisecond, clone.Configuration().RetryConfiguration.RetryWaitMax)
 	assert.Equal(t, "test-token", clone.clientRequestModifiers.headers.token)
 	assert.Equal(t, "test-namespace", clone.clientRequestModifiers.headers.namespace)
+}
+
+// We cannot compare the two objects directly since they have nested func pointers
+func testEqual(t *testing.T, c1 *Client, c2 *Client) {
+	assert.Equal(t, fmt.Sprintf("%v", c1.configuration), fmt.Sprintf("%v", c2.configuration))
+	assert.Equal(t, fmt.Sprintf("%v", c1.parsedBaseAddress), fmt.Sprintf("%v", c2.parsedBaseAddress))
+	assert.Equal(t, fmt.Sprintf("%v", c1.client), fmt.Sprintf("%v", c2.client))
+	assert.Equal(t, fmt.Sprintf("%v", c1.clientWithRetries), fmt.Sprintf("%v", c2.clientWithRetries))
+	assert.Equal(t, fmt.Sprintf("%v", c1.clientRequestModifiers), fmt.Sprintf("%v", c2.clientRequestModifiers))
+	assert.Equal(t, fmt.Sprintf("%v", c1.replicationStates.states), fmt.Sprintf("%v", c2.replicationStates.states))
 }

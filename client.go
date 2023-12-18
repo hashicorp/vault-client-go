4cb75eaf72a47ddb0f3d5dbd970460ae4648c854
Anton Averchenkov <84287187+averche@users.noreply.github.com>
2023-12-18 18:34:18 -0500

    0.4.3 release notes (#248)
    * 0.4.3 Changelog
    
    * typos

--- client.go
+++ client.go
@@ -18,7 +18,7 @@ import (
 	"github.com/hashicorp/go-retryablehttp"
 )
 
-const ClientVersion = "0.4.2"
+const ClientVersion = "0.4.3"
 
 // Client manages communication with Vault, initialize it with vault.New(...)
 type Client struct {
